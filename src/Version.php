acbcd5d8c4f649d60472d16b4c4bddcf82bab500
Jerry Price <jerryprice@lmo.com>
2016-07-25 13:01:10 -0400

    init commit

---
+++ src/Version.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppVersion;
+
+class Version 
+{
+    public static function current()
+    {
+        echo 'test-current';
+    }  
+
+    public static function previous()
+    {
+        echo 'test-previous';
+    }
+}
